1515a28e7833591840f56455b7acc082e77cf71e
Gage Bateman <gbateman@kyanicorp.com>
2021-08-16 12:51:24 -0600

    Updated footer template

--- themes/understrap/sidebar-templates/sidebar-footerfull.php
+++ themes/understrap/sidebar-templates/sidebar-footerfull.php
@@ -1,34 +1,46 @@
 <?php
+
 /**
  * Sidebar setup for footer full
  *
- * @package UnderStrap
+ * @package understrap
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
-<?php if ( is_active_sidebar( 'footerfull' ) ) : ?>
+<?php if (is_active_sidebar('footerfull')) : ?>
 
 	<!-- ******************* The Footer Full-width Widget Area ******************* -->
 
 	<div class="wrapper" id="wrapper-footer-full">
 
-		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
+		<div class="container" id="footer-full-content">
 
-			<div class="row">
+			<div class="row footer-menu">
 
-				<?php dynamic_sidebar( 'footerfull' ); ?>
+				<?php dynamic_sidebar('footerfull'); ?>
 
+			</div>
+
+			<div class="row footer-logo">
+				<?php dynamic_sidebar('footerlogo') ?>
+			</div>
+
+			<div class="row footer-social-icons">
+				<?php dynamic_sidebar("footersocial") ?>
+			</div>
+
+			<div class="row footer-additional-text">
+				<?php dynamic_sidebar('footertext') ?>
 			</div>
 
 		</div>
 
 	</div><!-- #wrapper-footer-full -->
 
-	<?php
-endif;
+<?php endif;
